c5bdd89c28666e8a6992f02366b8a0373b1f3bcc
Dinko <dinko.sulicek@predavaci.algebra.hr>
2025-05-14 21:09:13 +0200

    Prvi dio - kraj

--- src/main/java/org/example/model/Author.java
+++ src/main/java/org/example/model/Author.java
@@ -1,9 +1,16 @@
 package org.example.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
 
